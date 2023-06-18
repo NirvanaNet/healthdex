065ec1ec7849dea75a3c3d2893b8eb54d996c9ef
Daksh Singh Rathore <dakshrathore1729@gmail.com>
2023-06-18 10:44:23 +0530

    feat: Homepage Setup

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
+import { Lora } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const lora = Lora({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Create Next App',
@@ -11,7 +11,8 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+   
+      <body className={lora.className}>{children}</body>
     </html>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,11 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        // these are the theme colors don't change them
+        primary: '#5A63F0',
+        secondary: '#000513',
+      },
     },
   },
   plugins: [],
