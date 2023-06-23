7c4d6ce2119dc6c3b407ce7928390f0167203ed1
Daksh Singh Rathore <dakshrathore1729@gmail.com>
2023-06-24 00:36:53 +0530

    feat: dashboard is ready

--- next.config.js
+++ next.config.js
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'github.com',
-      },
-    ],
+  domains: [
+    'github.com',
+    'images.unsplash.com',
+  ],
   },}
 
 module.exports = nextConfig
