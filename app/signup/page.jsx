c9fab6b424e1513241bb31304e7d3d6468296998
priyankarpal <88102392+priyankarpal@users.noreply.github.com>
2023-06-30 19:05:44 +0530

    feat: sign in page added

--- app/signup/page.jsx
+++ app/signup/page.jsx
@@ -1,30 +1,31 @@
-"use client"
-import React from 'react'
+"use client";
+import React from "react";
 // import { useState } from 'react'
-import { Tab } from '@headlessui/react'
-import Navbar from '../../components/navbar'
+import { Tab } from "@headlessui/react";
+import Navbar from "../../components/navbar";
+import Link from "next/link";
 
 function classNames(...classes) {
-  return classes.filter(Boolean).join(' ')
+  return classes.filter(Boolean).join(" ");
 }
 
-const Signinpage = () => {
+const SignUpPage = () => {
   return (
-    <div className='overflow-x-hidden'>
+    <div className="overflow-x-hidden">
       <Navbar />
-      <section className='w-[100vw] h-[100%] flex justify-center overflow-x-hidden'>
-        <div className='w-[50vw] h-[100%] m-10 flex justify-center items-center border rounded-lg border-gray-500'>
-          <div className="w-[80%] h-[100%] px-6 py-6 sm:px-0">
-            <Tab.Group>
+      <section className="w-[100vw] h-[100%] flex justify-center overflow-x-hidden">
+        <div className="w-[50vw] h-[100%] m-10 flex justify-center items-center border rounded-lg border-gray-500">
+          <div className="w-[80%] h-[100%] px-6 py-6 sm:px-0 ">
+            <Tab.Group >
               <Tab.List className="flex space-x-10 rounded-xl bg-black p-1">
                 <Tab
                   className={({ selected }) =>
                     classNames(
-                      'w-full rounded-lg py-2.5 text-sm font-medium leading-5 text-black border outline-none duration-200  ease-in-out',
-                      'ring-primary ring-opacity-60 outline-none ring-offset-2 ring-offset-primary focus:outline-none focus:ring-2',
+                      "w-full rounded-lg py-2.5 text-sm font-medium leading-5 text-black border outline-none duration-200  ease-in-out",
+                      "ring-primary ring-opacity-60 outline-none ring-offset-2 ring-offset-primary focus:outline-none focus:ring-2",
                       selected
-                        ? 'bg-white text-black  shadow'
-                        : 'text-white hover:bg-white/[0.12] hover:text-white'
+                        ? "bg-white text-black  shadow"
+                        : "text-white hover:bg-white/[0.12] hover:text-white"
                     )
                   }
                 >
@@ -33,117 +34,160 @@ const Signinpage = () => {
                 <Tab
                   className={({ selected }) =>
                     classNames(
-                      'w-full rounded-lg py-2.5 text-sm font-medium leading-5 text-black border outline-none duration-200  ease-in-out ',
-                      'ring-primary ring-opacity-60 outline-none ring-offset-2 ring-offset-primary focus:outline-none focus:ring-2',
+                      "w-full rounded-lg py-2.5 text-sm font-medium leading-5 text-black border outline-none duration-200  ease-in-out ",
+                      "ring-primary ring-opacity-60 outline-none ring-offset-2 ring-offset-primary focus:outline-none focus:ring-2",
                       selected
-                        ? 'bg-white text-black  shadow'
-                        : 'text-white hover:bg-white/[0.12] hover:text-white'
+                        ? "bg-white text-black  shadow"
+                        : "text-white hover:bg-white/[0.12] hover:text-white"
                     )
                   }
                 >
                   Sign Up as a Patient
                 </Tab>
                 {/* <Tab
-              className={({ selected }) =>
+               className={({ selected }) =>
                 classNames(
                   'w-full rounded-lg py-2.5 text-sm font-medium leading-5 text-white',
                   'ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2',
                   selected
                     ? 'bg-white text-gray-800 shadow'
                     : 'text-white hover:bg-white/[0.12] hover:text-white'
-                )
-              }
-            >
-              Login
-            </Tab> */}
+                  )
+                }
+               >
+               Login
+             </Tab> */}
               </Tab.List>
               <Tab.Panels className="mt-5">
-                <Tab.Panel className=' '>
-                  <div className='w-full h-[100%] bg-black rounded-md mt-15 flex justify-center items-center'>
-                    <form className='text-white h-[100%] mt-15 p-5 ml-8 w-full  '>
-                      <label className='block mt-5'>
-                        Doctor ID
-                      </label>
-                      <input type="text" className='bg-gray-800 block mt-3 w-full text-white  rounded outline-none p-2' placeholder='Enter your doctor ID' />
-                      <label className='block mt-5'>
-                        Doctor Name
-                      </label>
-                      <input type="text" className='bg-gray-800 block mt-3 w-full text-white  rounded outline-none p-2' placeholder='Enter your full name' />
-                      <label className='block mt-5'>
-                        Specialization
-                      </label>
-                      <input type="text" className='bg-gray-800 block mt-3 w-full text-white  rounded outline-none p-2' placeholder='What is your specialization ' />
-                      <label className='block mt-5'>
-                        Address
-                      </label>
-                      <input type="text" className='bg-gray-800 block mt-3 w-full text-white  rounded outline-none p-2' placeholder='Enter your address' />
-                      <label className='block mt-5'>
-                        Contact Number
-                      </label>
-                      <input type="text" className='bg-gray-800 block mt-3 w-full text-white  rounded outline-none p-2' placeholder='Enter your mobile number' />
+                <Tab.Panel className=" ">
+                  <div className="w-full h-[100%] bg-black rounded-md mt-15 flex justify-center items-center">
+                    <form className="text-white h-[100%] mt-15 p-5 ml-8 w-full  ">
+                      <label className="block mt-5">Doctor ID</label>
+                      <input
+                        type="text"
+                        className="bg-gray-800 block mt-3 w-full text-white  rounded outline-none p-2"
+                        placeholder="Enter your doctor ID"
+                      />
+                      <label className="block mt-5">Doctor Name</label>
+                      <input
+                        type="text"
+                        className="bg-gray-800 block mt-3 w-full text-white  rounded outline-none p-2"
+                        placeholder="Enter your full name"
+                      />
+                      <label className="block mt-5">Specialization</label>
+                      <input
+                        type="text"
+                        className="bg-gray-800 block mt-3 w-full text-white  rounded outline-none p-2"
+                        placeholder="What is your specialization "
+                      />
+                      <label className="block mt-5">Address</label>
+                      <input
+                        type="text"
+                        className="bg-gray-800 block mt-3 w-full text-white  rounded outline-none p-2"
+                        placeholder="Enter your address"
+                      />
+                      <label className="block mt-5">Contact Number</label>
+                      <input
+                        type="text"
+                        className="bg-gray-800 block mt-3 w-full text-white  rounded outline-none p-2"
+                        placeholder="Enter your mobile number"
+                      />
 
-                      <label className='block mt-5'>
-                        Email Address</label>
-                      <input type="email" className='bg-gray-800 block mt-3 w-full text-white  rounded outline-none p-2' placeholder='Enter your email address' />
-                      <label className='block mt-5'>
-                        Create Password</label>
-                      <input type="password" className='bg-gray-800 block mt-3 w-full text-white  rounded outline-none p-2' placeholder='Create a password' />
-                      <button className='px-5 py-2  rounded-lg mt-10 bg-white text-black hover:bg-gray-300 mx-[40%]'>Sign Up</button>
+                      <label className="block mt-5">Email Address</label>
+                      <input
+                        type="email"
+                        className="bg-gray-800 block mt-3 w-full text-white  rounded outline-none p-2"
+                        placeholder="Enter your email address"
+                      />
+                      <label className="block mt-5">Create Password</label>
+                      <input
+                        type="password"
+                        className="bg-gray-800 block mt-3 w-full text-white  rounded outline-none p-2"
+                        placeholder="Create a password"
+                      />
+                      <button className="px-5 py-2  rounded-lg mt-10 bg-white text-black hover:bg-gray-300 mx-[40%]">
+                        Sign Up
+                      </button>
                     </form>
                   </div>
-
                 </Tab.Panel>
                 {/*Patient Signup*/}
                 <Tab.Panel>
-                  <div className='w-[100%] h-[100%] bg-black rounded-md mt-15 '>
-                    <form className='text-white h-[100%] mt-15 p-5 ml-8 w-full'>
-                      <label className='block mt-5 '>
+                  <div className="w-[100%] h-[100%] bg-black rounded-md mt-15 ">
+                    <form className="text-white h-[100%] mt-15 p-5 ml-8 w-full">
+                      <label className="block mt-5 ">
                         Date of Registration
                       </label>
-                      <input type="datetime-local" className='bg-gray-800 block mt-3 w-full text-white  rounded outline-none p-2' />
-                      <label className='block mt-5'>
-                        Patient Name
-                      </label>
-                      <input type="text" className='bg-gray-800 block mt-3 w-full text-white  rounded outline-none p-2' placeholder='Enter your name' />
-                      <label className='block mt-5'>
-                        Age
-                      </label>
-                      <input type="text" className='bg-gray-800 block mt-3 w-full text-white  rounded outline-none p-2' placeholder='Enter your age' />
-                      <label className='block mt-5'>
-                        Gender
-                      </label>
-                      <input type="text" className='bg-gray-800 block mt-3 w-full text-white  rounded outline-none p-2' placeholder='Enter your gender' />
-                      <label className='block mt-5'>
-                        Address
-                      </label>
-                      <input type="text" className='bg-gray-800 block mt-3 w-full text-white  rounded outline-none p-2' placeholder='Enter your address' />
-                      <label className='block mt-5'>
-                        Contact Number
-                      </label>
-                      <input type="text" className='bg-gray-800 block mt-3 w-full text-white  rounded outline-none p-2' placeholder='Enter your contact number' />
+                      <input
+                        type="datetime-local"
+                        className="bg-gray-800 block mt-3 w-full text-white  rounded outline-none p-2"
+                      />
+                      <label className="block mt-5">Patient Name</label>
+                      <input
+                        type="text"
+                        className="bg-gray-800 block mt-3 w-full text-white  rounded outline-none p-2"
+                        placeholder="Enter your name"
+                      />
+                      <label className="block mt-5">Age</label>
+                      <input
+                        type="text"
+                        className="bg-gray-800 block mt-3 w-full text-white  rounded outline-none p-2"
+                        placeholder="Enter your age"
+                      />
+                      <label className="block mt-5">Gender</label>
+                      <input
+                        type="text"
+                        className="bg-gray-800 block mt-3 w-full text-white  rounded outline-none p-2"
+                        placeholder="Enter your gender"
+                      />
+                      <label className="block mt-5">Address</label>
+                      <input
+                        type="text"
+                        className="bg-gray-800 block mt-3 w-full text-white  rounded outline-none p-2"
+                        placeholder="Enter your address"
+                      />
+                      <label className="block mt-5">Contact Number</label>
+                      <input
+                        type="text"
+                        className="bg-gray-800 block mt-3 w-full text-white  rounded outline-none p-2"
+                        placeholder="Enter your contact number"
+                      />
 
-                      <label className='block mt-5'>
-                        Email Address</label>
-                      <input type="email" className='bg-gray-800 block mt-3 w-full text-white  rounded outline-none p-2' placeholder='Enter your email address' />
-                      <label className='block mt-5'>
-                        Create Password</label>
-                      <input type="password" className='bg-gray-800 block mt-3 w-full text-white  rounded outline-none p-2' placeholder='Create a password' />
+                      <label className="block mt-5">Email Address</label>
+                      <input
+                        type="email"
+                        className="bg-gray-800 block mt-3 w-full text-white  rounded outline-none p-2"
+                        placeholder="Enter your email address"
+                      />
+                      <label className="block mt-5">Create Password</label>
+                      <input
+                        type="password"
+                        className="bg-gray-800 block mt-3 w-full text-white  rounded outline-none p-2"
+                        placeholder="Create a password"
+                      />
 
-
-                      <button className='px-5 py-2 rounded-lg mt-10  bg-white text-black hover:bg-gray-30 mx-[40%]'>Sign Up</button>
+                      <button className="px-5 py-2 rounded-lg mt-10  bg-white text-black hover:bg-gray-30 mx-[40%]">
+                        Sign Up
+                      </button>
                     </form>
                   </div>
+
                 </Tab.Panel>
                 {/* <Tab.Panel>
                 Login
-            </Tab.Panel> */}
+              </Tab.Panel> */}
+
               </Tab.Panels>
             </Tab.Group>
+            <div className="flex justify-center items-center">
+
+              <p>already have an account? <Link href="/signin" className="underline text-primary ">sign in here </Link> </p>
+            </div>
           </div>
         </div>
       </section>
     </div>
-  )
-}
+  );
+};
 
-export default Signinpage
+export default SignUpPage;
