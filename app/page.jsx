072b3d3a9a782a7a71d0cc80cbd6e260400a9b13
Piyush Jha <73685420+piyushjha0409@users.noreply.github.com>
2023-07-08 18:50:24 +0530

    changes applied all the authentication is done in the frontend side (#23)

--- app/page.jsx
+++ app/page.jsx
@@ -1,78 +1,57 @@
-"use client" // this is a client-side only component
-import { useState, useEffect } from 'react'
-import { Dialog, Transition } from '@headlessui/react'
-import { Fragment } from 'react'
-import Navbar from "../components/navbar"
-import * as fcl from "@onflow/fcl"
-import * as t from "@onflow/types"
+"use client"; // this is a client-side only component
+import { useState, useEffect } from "react";
+import { Dialog, Transition } from "@headlessui/react";
+import { Fragment } from "react";
+import Navbar from "../components/navbar";
+import * as fcl from "@onflow/fcl";
+import * as t from "@onflow/types";
 import { config } from "@onflow/fcl";
-import { useRouter } from 'next/navigation'
-import { RouteHandlerManager } from 'next/dist/server/future/route-handler-managers/route-handler-manager'
-
+import { useRouter } from "next/navigation";
+import { RouteHandlerManager } from "next/dist/server/future/route-handler-managers/route-handler-manager";
 
 config({
   "accessNode.api": "https://rest-testnet.onflow.org", // Mainnet: "https://rest-mainnet.onflow.org"
-  "discovery.wallet": "https://fcl-discovery.onflow.org/testnet/authn" // Mainnet: "https://fcl-discovery.onflow.org/authn"
-})
-
+  "discovery.wallet": "https://fcl-discovery.onflow.org/testnet/authn", // Mainnet: "https://fcl-discovery.onflow.org/authn"
+});
 
 export default function Home() {
-
-  const [user, setUser] = useState({addr: ''}) 
-  let [isOpen, setIsOpen] = useState(false) // for popup
+  const [isOpen, setIsOpen] = useState(false); // for popup
 
   // functions for popup
 
   function closeModal() {
-    setIsOpen(false)
+    setIsOpen(false);
   }
 
   function openModal() {
-    setIsOpen(true)
+    setIsOpen(true);
   }
 
-  
- 
-  const router = useRouter()
+  const router = useRouter();
 
-  useEffect(()=>{
-    fcl.currentUser.subscribe(setUser)
-  })
-  
-  //function for login 
+  //function for login
   const Login = async () => {
-    try{
-    await fcl.authenticate().then(()=>{
-      fcl.currentUser.subscribe(setUser)
-      localStorage.setItem("User", user.addr),  
-      router.push("/signin")
-    })
-    }catch(err){
-      console.error(err)
-    }
-  }
-  
- 
-  //function for logout
-
-  const Logout = () =>{
-    try{
-      fcl.unauthenticate().then(()=>{
-        localStorage.removeItem("User")
-        router.push("/")
+    await fcl.authenticate().then(() => {
+      fcl.currentUser.subscribe((currentUser)=> {
+        localStorage.setItem("User", currentUser.addr.toString())
       })
-    }catch(err){
-      console.error(err)
-    }
-  }
+      router.push("/signin")
+    });
+  };
 
-  // //useEffect
-  // useEffect(()=> {
-  //  if(user.addr !== ''){
-  //  }else if(user.addr === null){
+  const user = localStorage.getItem("User")
 
-  //  }
-  // }, [])
+  useEffect(()=> {
+    console.log(user)
+  })
+
+
+  //function for logout
+  const Logout = () => {
+    fcl.unauthenticate()
+    localStorage.removeItem("User");
+    window.location.reload()
+  };
 
   return (
     <main className=" text-white">
@@ -80,41 +59,44 @@ export default function Home() {
       <Navbar />
       {/* HeroPage */}
       <section className=" text-white ">
-        {user.addr ? user.addr : ""}
-        <div
-          className="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen  "
-        >
+        <div className="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen  ">
           <div className="mx-auto max-w-3xl text-center  ">
-            <h1
-              className="bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text text-3xl font-extrabold text-transparent sm:text-6xl"
-            >
+            <h1 className="bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 bg-clip-text text-3xl font-extrabold text-transparent sm:text-6xl">
               healthdex for secure healthcare records
-
             </h1>
 
             <p className="mx-auto mt-8 w-50 text-xl">
-              HealthDex is a reliable and robust platform, its advanced features and stringent protocols offers a seamless and protected environment for storing and accessing sensitive medical information.
+              HealthDex is a reliable and robust platform, its advanced features
+              and stringent protocols offers a seamless and protected
+              environment for storing and accessing sensitive medical
+              information.
             </p>
 
-      {/* conditional renedering  */}
+            {/* conditional renedering  */}
 
-           { user.addr === "" && <div className="mt-8 flex flex-wrap justify-center gap-4 ">
-              <button onClick={Login} type='button'
-                className="block w-full rounded border border-blue-600 bg-blue-600 px-12 py-3 text-md font-medium text-white    hover:scale-110 duration-300  sm:w-auto"
-              >
-                Connect with Wallet
-              </button>
-
-            </div>
-            }
-            <div className="mt-8 flex flex-wrap justify-center gap-4 ">
-              <button onClick={Logout} type='button'
+            {!user ? 
+              (<div className="mt-8 flex flex-wrap justify-center gap-4 ">
+                <button
+                  onClick={Login}
+                  type="button"
+                  className="block w-full rounded border border-blue-600 bg-blue-600 px-12 py-3 text-md font-medium text-white    hover:scale-110 duration-300  sm:w-auto"
+                >
+                  Connect with Wallet
+                </button>
+              </div>
+            ) :
+            (
+              <div className="mt-8 flex flex-wrap justify-center gap-4 ">
+              <button
+                onClick={Logout}
+                type="button"
                 className="block w-full rounded border border-blue-600 bg-blue-600 px-12 py-3 text-md font-medium text-white    hover:scale-110 duration-300  sm:w-auto"
               >
                 Disconnect with Wallet
               </button>
-
             </div>
+            )
+          }     
           </div>
         </div>
       </section>
@@ -154,7 +136,8 @@ export default function Home() {
                   </Dialog.Title>
                   <div className="mt-2">
                     <p className="text-sm text-gray-500">
-                      This feature is coming soon. We are working on it. Stay tuned!
+                      This feature is coming soon. We are working on it. Stay
+                      tuned!
                     </p>
                   </div>
 
@@ -174,5 +157,5 @@ export default function Home() {
         </Dialog>
       </Transition>
     </main>
-  )
+  );
 }
